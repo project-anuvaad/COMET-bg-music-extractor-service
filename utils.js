b277ee24c38d61c9a9803716554ed37da67a60c9
hassanamin994 <hassanmamin994@gmail.com>
2020-04-12 23:14:14 +0200

    add eslint

--- utils.js
+++ utils.js
@@ -2,13 +2,13 @@ const { exec } = require('child_process');
 const fs = require('fs');
 // silence threashold in seconds
 // slide duration threashold in seconds
-const SLIDE_THREASHOLD = 10;
+// const SLIDE_THREASHOLD = 10;
 const async = require('async')
 
 function downloadFile(url, targetPath) {
     return new Promise((resolve, reject) => {
         // https://tailoredvideowiki.s3.eu-west-1.amazonaws.com/videos/1.mp4
-        exec(`curl ${url} --output ${targetPath}`, (err, stdout, stderr) => {
+        exec(`curl ${url} --output ${targetPath}`, (err) => {
             if (err) {
                 return reject(err);
             }
@@ -21,52 +21,52 @@ function downloadFile(url, targetPath) {
     })
 }
 
-function divideSpeakerSlidesByDot(slide) {
-    const itemsDuration = parseFloat(slide.items[slide.items.length - 1].end_time) - parseFloat(slide.items[0].start_time);
-    if (itemsDuration <= SLIDE_THREASHOLD) return [slide];
-    const newSlides = [];
-    slide.items.forEach((item) => {
-        item.start_time = parseFloat(item.start_time);
-        item.end_time = parseFloat(item.end_time);
-    })
-    let timeSum = slide.items[0].end_time - slide.items[0].start_time;
-    let firstItem = slide.items[0];
-    let lastItem;
-    let content = slide.items[0].alternatives[0].content;
-    slide.items.forEach((item, itemIndex) => {
-        if (item.type !== 'punctuation') {
-            timeSum += (item.end_time - item.start_time);
-        }
-        if (itemIndex === 0) return;
-        if (item.type === 'punctuation') {
-            content += `${item.alternatives[0].content}`
-        } else {
-            lastItem = item;
-            content += ` ${item.alternatives[0].content}`
-        }
-        if (item.alternatives[0].content === '.' && timeSum >= 10) {
-            newSlides.push({
-                speakerLabel: slide.speakerLabel,
-                startTime: firstItem.start_time,
-                endTime: lastItem.end_time,
-                content,
-            })
-            content = ''
-            firstItem = slide.items[itemIndex + 1];
-            timeSum = 0
-        } else if (itemIndex === (slide.items.length - 1)) {
-            newSlides.push({
-                speakerLabel: slide.speakerLabel,
-                startTime: firstItem.start_time,
-                endTime: lastItem.end_time,
-                content,
-            })
-        }
+// function divideSpeakerSlidesByDot(slide) {
+//     const itemsDuration = parseFloat(slide.items[slide.items.length - 1].end_time) - parseFloat(slide.items[0].start_time);
+//     if (itemsDuration <= SLIDE_THREASHOLD) return [slide];
+//     const newSlides = [];
+//     slide.items.forEach((item) => {
+//         item.start_time = parseFloat(item.start_time);
+//         item.end_time = parseFloat(item.end_time);
+//     })
+//     let timeSum = slide.items[0].end_time - slide.items[0].start_time;
+//     let firstItem = slide.items[0];
+//     let lastItem;
+//     let content = slide.items[0].alternatives[0].content;
+//     slide.items.forEach((item, itemIndex) => {
+//         if (item.type !== 'punctuation') {
+//             timeSum += (item.end_time - item.start_time);
+//         }
+//         if (itemIndex === 0) return;
+//         if (item.type === 'punctuation') {
+//             content += `${item.alternatives[0].content}`
+//         } else {
+//             lastItem = item;
+//             content += ` ${item.alternatives[0].content}`
+//         }
+//         if (item.alternatives[0].content === '.' && timeSum >= 10) {
+//             newSlides.push({
+//                 speakerLabel: slide.speakerLabel,
+//                 startTime: firstItem.start_time,
+//                 endTime: lastItem.end_time,
+//                 content,
+//             })
+//             content = ''
+//             firstItem = slide.items[itemIndex + 1];
+//             timeSum = 0
+//         } else if (itemIndex === (slide.items.length - 1)) {
+//             newSlides.push({
+//                 speakerLabel: slide.speakerLabel,
+//                 startTime: firstItem.start_time,
+//                 endTime: lastItem.end_time,
+//                 content,
+//             })
+//         }
 
-    })
+//     })
 
-    return newSlides;
-}
+//     return newSlides;
+// }
 
 function getRemoteFileDuration(url) {
     return new Promise((resolve, reject) => {

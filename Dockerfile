880164f01429a755280f2c84f9fafdcbc97fa475
hassanamin994 <hassanmamin994@gmail.com>
2020-07-19 02:57:48 +0200

    add healthcheck route

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,6 @@ RUN npm install
 
 COPY . .
 
+EXPOSE 4000
 CMD ["npm", "run", "docker:prod"]
+HEALTHCHECK --start-period=30s --interval=2m CMD curl -sS localhost:4000/health || exit 1
--- worker.js
+++ worker.js
@@ -1,5 +1,6 @@
 
 const fs = require('fs');
+const videowikiGenerators = require('@videowiki/generators');
 
 const RABBITMQ_SERVER = process.env.RABBITMQ_SERVER;
 
@@ -30,15 +31,29 @@ try {
 let channel;
 rabbitmqService.createChannel(RABBITMQ_SERVER, (err, ch) => {
     if (err) throw err;
-    console.log('connected to ', RABBITMQ_SERVER, EXTRACT_VIDEO_BACKGROUND_MUSIC_QUEUE, process.env.MODEL_DIRECTORY)
+    console.log('RABBITMQ CONNECTED')
     channel = ch;
     channel.prefetch(1)
     channel.assertQueue(EXTRACT_VIDEO_BACKGROUND_MUSIC_QUEUE, { durable: true });
     channel.assertQueue(EXTRACT_VIDEO_BACKGROUND_MUSIC_FINISH_QUEUE, { durable: true });
 
     channel.consume(EXTRACT_VIDEO_BACKGROUND_MUSIC_QUEUE, onExtractVideoBackgroundMusic(channel), { noAck: false });
-    
+
+    // Exit app on channel error
+    channel.on('error', (err) => {
+        console.log('RABBITMQ ERROR', err)
+        process.exit(1);
+    })
+    channel.on('close', () => {
+        console.log('RABBITMQ CLOSE')
+        process.exit(1);
+    }) 
     setTimeout(() => {
         // channel.sendToQueue(CONVERT_VIDEO_TO_ARTICLE_QUEUE, new Buffer(JSON.stringify({ videoId: "5d6d58e54be12b2d18b22b58", articleId: '5d6d5a1e03f9fa6cb96cc2ee' })));
     }, 2000);
+
+    const { app, server } = videowikiGenerators.serverGenerator({ uploadLimit: 50 });
+    videowikiGenerators.healthcheckRouteGenerator({ router: app, rabbitmqConnection: channel.connection });
+    server.listen(4000)
+
 })
